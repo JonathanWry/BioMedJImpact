3bbaab154048e3f2142eb9c8d0386552e6a9315c
Jonathan Wang <112515982+JonathanWry@users.noreply.github.com>
2025-11-12 18:18:43 -0500

    updated code

--- vllm_extraction_i.sh
+++
@@ -1,9 +0,0 @@
-# 初始化 Conda
-conda init bash > /dev/null 2>&1
-source ~/.bashrc
-
-# 激活指定 Conda 环境
-conda activate gemma  # 替换为你的 Conda 环境名
-
-# 执行 Python 脚本
-python code/vllm_keyword_extraction.py --OA_SECTION "04"       # 替换为你的 Python 脚本名
